7d194ce37561a78efe0b8457cec866e55b74c1b2
Ejektaflex <dootydooter@gmail.com>
2021-03-11 12:12:37 -0600

    Work on Maven publishing

--- src/test/java/io/ejekta/testing/Main.kt
+++ src/test/java/io/ejekta/testing/Main.kt
@@ -1,11 +1,18 @@
 @file:Suppress("EXPERIMENTAL_API_USAGE")
 package io.ejekta.testing
 
+import io.ejekta.kambrik.ext.wrapToPacketByteBuf
 import io.ejekta.kambrikx.api.serial.nbt.NbtFormat
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.minecraft.nbt.ListTag
+import net.minecraft.nbt.LongArrayTag
+import net.minecraft.nbt.LongTag
 import net.minecraft.util.math.BlockPos
 
 val serMod = SerializersModule {
@@ -60,7 +67,6 @@ fun main(args: Array<String>) {
 
     val asObj = NbtFormatTest.decodeFromTag<SlingshotData>(asNbt)
     println("As Obj: $asObj")
-
 
 }
 
