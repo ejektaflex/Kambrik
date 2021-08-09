4215e0c70ff907f599687555d8107d2e78d45619
Ejektaflex <dootydooter@gmail.com>
2021-08-09 17:05:42 -0500

    Simplify client network logic

--- src/main/java/io/ejekta/kambrik/api/network/NetworkLinker.kt
+++ src/main/java/io/ejekta/kambrik/api/network/NetworkLinker.kt
@@ -2,7 +2,9 @@ package io.ejekta.kambrik.api.network
 
 import io.ejekta.kambrik.api.network.client.ClientMsg
 import io.ejekta.kambrik.api.network.client.ClientNetworkLink
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.serializer
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Identifier
 import kotlin.reflect.KClass
@@ -15,18 +17,13 @@ object NetworkLinker {
     inline fun <reified S : ClientMsg<S>> linkClientMsg(ser: KSerializer<S>, id: Identifier): ClientNetworkLink<S> {
         val linkage = ClientNetworkLink(id, ser)
         linkage.register()
-
         typeLinks[S::class] = linkage
-
         return linkage
     }
 
-    inline fun <reified M : ClientMsg<M>> sendClientMsg(msg: M, player: ServerPlayerEntity) {
-
+    fun <M : ClientMsg<M>> sendClientMsg(msg: M, player: ServerPlayerEntity) {
         val link = typeLinks[msg::class]!! as ClientNetworkLink<M>
-
         println("Link: $link")
-
         link.send(msg, player)
     }
 
