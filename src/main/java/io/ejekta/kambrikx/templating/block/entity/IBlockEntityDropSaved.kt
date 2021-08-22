916fa246e1741de76760031e313e98121211a3b7
Ejektaflex <dootydooter@gmail.com>
2021-08-22 18:48:03 -0500

    Move not yet ready template to experimental

---
+++ src/main/java/io/ejekta/kambrikx/templating/block/entity/IBlockEntityDropSaved.kt
@@ -0,0 +1,57 @@
+package io.ejekta.kambrikx.templating.block.entity
+
+import io.ejekta.kambrik.internal.KambrikExperimental
+import net.minecraft.block.AbstractBlock
+import net.minecraft.block.BlockState
+import net.minecraft.block.BlockWithEntity
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.entity.LivingEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.loot.context.LootContext
+import net.minecraft.loot.context.LootContextParameters
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+/**
+ * Used for blocks which save their NBT data into their respective itemstack
+ */
+@KambrikExperimental
+abstract class KambrikBlockWithEntitySavesDrop(settings: Settings) : BlockWithEntity(settings) {
+
+    abstract fun getBlockEntityType(): BlockEntityType<*>?
+
+    abstract val entitySubTagName: String
+
+    override fun getDroppedStacks(state: BlockState?, builder: LootContext.Builder?): MutableList<ItemStack> {
+        val blockEntity = builder?.getNullable(LootContextParameters.BLOCK_ENTITY)
+        if (blockEntity?.type == getBlockEntityType()) {
+            return super.getDroppedStacks(state, builder).map {
+                it.apply {
+                    blockEntity!!.writeNbt(orCreateNbt.getCompound(entitySubTagName))
+                }
+            }.toMutableList()
+        }
+        return mutableListOf()
+    }
+
+    override fun onPlaced(
+        world: World?,
+        pos: BlockPos?,
+        state: BlockState?,
+        placer: LivingEntity?,
+        itemStack: ItemStack?
+    ) {
+        if (world != null && pos != null && itemStack != null && !world.isClient) {
+            val blockEntity = world.getBlockEntity(pos, getBlockEntityType())
+            blockEntity.ifPresent {
+                val itemNbt = itemStack.nbt ?: return@ifPresent
+                if (itemNbt.contains(entitySubTagName)) {
+                    val tag = itemNbt.getCompound(entitySubTagName)
+                    it.readNbt(tag)
+                    it.markDirty()
+                }
+            }
+        }
+    }
+
+}
