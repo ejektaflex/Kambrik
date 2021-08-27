9a4c2cfa5050378adfb2a0a57c0c0d9ce1dccf5e
Ejektaflex <dootydooter@gmail.com>
2021-08-26 22:06:55 -0500

    Simplify keybinding API
    This new keybinding API is great! I made a few tweaks to simplify the experience:
    * The method took in an enum with two possible values, and they simply get substituted for other values. I simplied this and made two functions (one for keyboard and one for mouse)
    * These builders all take the same values, and order doesn't matter, but since they're all required they should be inlined into the method parameters. We unfortunately don't gain much by specifying these required paramers in a nondescript order

--- src/main/java/io/ejekta/kambrikx/input/KambrikInputApi.kt
+++ src/main/java/io/ejekta/kambrikx/input/KambrikInputApi.kt
@@ -1,11 +1,50 @@
 package io.ejekta.kambrikx.input
 
-import io.ejekta.kambrik.internal.KambrikExperimental
-import io.ejekta.kambrikx.input.keybind.KambrikKeybindingApi
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
 
 class KambrikInputApi internal constructor() {
 
-    @OptIn(KambrikExperimental::class)
-    val Keybinding = KambrikKeybindingApi()
+    private fun registerBinding(
+        key: Int,
+        keyTranslation: String,
+        keyCategory: String,
+        type: InputUtil.Type,
+        onPressed: () -> Unit
+    ) {
+        val bind = KeyBindingHelper.registerKeyBinding(
+            KeyBinding(
+            keyTranslation,
+            type,
+            key,
+            keyCategory
+        )
+        )
+
+        ClientTickEvents.END_CLIENT_TICK.register {
+            if (bind.wasPressed()) {
+                onPressed()
+            }
+        }
+
+    }
+
+    fun registerKeyboardBinding(
+        key: Int = GLFW.GLFW_KEY_UNKNOWN,
+        keyTranslation: String,
+        keyCategory: String,
+        onPressed: () -> Unit
+    ) = registerBinding(key, keyTranslation, keyCategory, InputUtil.Type.KEYSYM, onPressed)
+
+    fun registerMouseBinding(
+        key: Int = GLFW.GLFW_KEY_UNKNOWN,
+        keyTranslation: String,
+        keyCategory: String,
+        onPressed: () -> Unit
+    ) = registerBinding(key, keyTranslation, keyCategory, InputUtil.Type.MOUSE, onPressed)
+
 
 }
