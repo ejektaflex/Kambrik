a51ad8ad96f45451a754d9ab4c0360ac87c31278
E F <dootydooter@gmail.com>
2022-05-14 16:33:44 -0500

    Rework text classes

--- src/main/java/io/ejekta/kambrik/text/TextBuilderDSL.kt
+++ src/main/java/io/ejekta/kambrik/text/TextBuilderDSL.kt
@@ -7,33 +7,33 @@ import net.minecraft.util.Formatting
 import java.util.*
 
 
-fun textLiteral(str: String = "", func: KambrikTextBuilder<LiteralText>.() -> Unit = {}): LiteralText {
-    return textBuilder(LiteralText(str), func)
+fun textLiteral(str: String = "", func: KambrikTextBuilder<MutableText>.() -> Unit = {}): MutableText {
+    return textBuilder(MutableText.of(LiteralTextContent(str)), func)
 }
 
-fun textTranslate(key: String, func: KambrikTextBuilder<TranslatableText>.() -> Unit = {}): TranslatableText {
-    return textBuilder(TranslatableText(key), func)
+fun textTranslate(key: String, func: KambrikTextBuilder<MutableText>.() -> Unit = {}): MutableText {
+    return textBuilder(MutableText.of(TranslatableTextContent(key)), func)
 }
 
-fun textKeybind(key: String, func: KambrikTextBuilder<KeybindText>.() -> Unit = {}): KeybindText {
-    return textBuilder(KeybindText(key), func)
+fun textKeybind(key: String, func: KambrikTextBuilder<MutableText>.() -> Unit = {}): MutableText {
+    return textBuilder(MutableText.of(KeybindTextContent(key)), func)
 }
 
-fun textScore(name: String, objective: String, func: KambrikTextBuilder<ScoreText>.() -> Unit = {}): ScoreText {
-    return textBuilder(ScoreText(name, objective), func)
+fun textScore(name: String, objective: String, func: KambrikTextBuilder<MutableText>.() -> Unit = {}): MutableText {
+    return textBuilder(MutableText.of(ScoreTextContent(name, objective)), func)
 }
 
-fun textSelector(pattern: String, separator: Text?, func: KambrikTextBuilder<SelectorText>.() -> Unit = {}): SelectorText {
-    return textBuilder(SelectorText(pattern, Optional.ofNullable(separator)), func)
+fun textSelector(pattern: String, separator: Text?, func: KambrikTextBuilder<MutableText>.() -> Unit = {}): MutableText {
+    return textBuilder(MutableText.of(SelectorTextContent(pattern, Optional.ofNullable(separator))), func)
 }
 
-internal fun <T : BaseText> textBuilder(starterText: T, func: KambrikTextBuilder<T>.() -> Unit): T {
+internal fun <T : MutableText> textBuilder(starterText: T, func: KambrikTextBuilder<T>.() -> Unit): T {
     val builder = KambrikTextBuilder(starterText)
     builder.func()
     return builder.root
 }
 
-class KambrikTextBuilder<T : BaseText>(
+class KambrikTextBuilder<T : MutableText>(
     var root: T
 ) {
 
@@ -95,7 +95,7 @@ class KambrikTextBuilder<T : BaseText>(
         hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, text)
     }
 
-    fun onHoverShowText(inFunc: KambrikTextBuilder<LiteralText>.() -> Unit) {
+    fun onHoverShowText(inFunc: KambrikTextBuilder<MutableText>.() -> Unit) {
         hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, textLiteral("", inFunc))
     }
 
@@ -109,19 +109,19 @@ class KambrikTextBuilder<T : BaseText>(
         return KambrikTextBuilder(this).apply(inFunc)
     }
 
-    operator fun String.invoke(inFunc: KambrikTextBuilder<LiteralText>.() -> Unit): LiteralText {
-        return KambrikTextBuilder(LiteralText(this)).apply(inFunc).root
+    operator fun String.invoke(inFunc: KambrikTextBuilder<MutableText>.() -> Unit): MutableText {
+        return KambrikTextBuilder(MutableText.of(LiteralTextContent(this))).apply(inFunc).root
     }
 
     fun add(text: Text) {
         root.append(text)
     }
 
-    fun addLiteral(str: String, func: KambrikTextBuilder<LiteralText>.() -> Unit = {}) {
+    fun addLiteral(str: String, func: KambrikTextBuilder<MutableText>.() -> Unit = {}) {
         root.append(textLiteral(str, func))
     }
 
-    fun addTranslate(key: String, func: KambrikTextBuilder<TranslatableText>.() -> Unit = {}) {
+    fun addTranslate(key: String, func: KambrikTextBuilder<MutableText>.() -> Unit = {}) {
         root.append(textTranslate(key, func))
     }
 
